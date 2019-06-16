4e36ec44638ade1f14b68c9c3bfc0b201d6721c4
Reet <reetmehta@hotmail.com>
2019-06-16 11:10:11 +0530

    new program

---
+++ substrings.py
@@ -0,0 +1,17 @@
+word=input("Enter the word:")
+w=[]
+for i in word:
+    w.append(i)
+n=0
+
+for i in range(0,len(w)):
+    counter=0
+    pointer=i
+    for j in range(0,len(w)-i):
+        if w[pointer] is w[j]:
+            pointer+=1
+            counter+=1
+        else: break
+    n+=counter
+
+print(n)
